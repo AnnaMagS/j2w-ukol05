3be250bb8b2dcb40a182d65f8725bb178e2649a2
18-1855-1 <annamagdalena.stranska@unicorncollege.cz>
2021-05-09 21:00:35 +0200

    First version of registration form

--- src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistraceController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistraceController.java
@@ -1,7 +1,11 @@
 package cz.czechitas.java2webapps.ukol5.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Kontroler obsluhující registraci účastníků dětského tábora.
@@ -10,8 +14,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class RegistraceController {
 
   @GetMapping("/")
-  public String formular() {
-    return "formular";
+  public ModelAndView formular() {
+    ModelAndView formular = new ModelAndView("formular");
+    formular.addObject("form",new RegistraceForm());
+    return formular;
+  }
+
+  @PostMapping("")
+  public Object form(@ModelAttribute("form") RegistraceForm form, BindingResult bindingResult) {
+    if (bindingResult.hasErrors()) {
+      return "/formular";
+    }
+
+/*    if (form.getVek() < 18) {
+      return "/alkohol/nizky-vek";
+
+      bindingResult.rejectValue("vek", "", "To by nešlo. Nejsi náhodou starší?");
+      return "/alkohol/formular";
+
+    }*/
+
+    return new ModelAndView("/registrovano")
+      //      .addObject("kod", Math.abs(random.nextInt()))
+            .addObject("email", form.getEmail());
   }
 
 }
---
+++ src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistraceForm.java
@@ -0,0 +1,109 @@
+package cz.czechitas.java2webapps.ukol5.controller;
+
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.EnumSet;
+
+public class RegistraceForm {
+
+    private String jmeno;
+    @NotNull
+    @NotBlank
+    private String prijmeni;
+    @NotNull
+    @NotBlank
+    private LocalDate vek;
+    @NotNull
+    @NotBlank
+    private Pohlavi pohlavi;
+    private Sport sport;
+    private String turnus;
+    private String email;
+    @Email
+    @NotBlank
+    private String telefon;
+
+
+
+    private EnumSet<Sport>vyber;
+
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public void setJmeno(String jmeno) {
+        this.jmeno = jmeno;
+    }
+
+    public String getPrijmeni() {
+        return prijmeni;
+    }
+
+    public void setPrijmeni(String prijmeni) {
+        this.prijmeni = prijmeni;
+    }
+
+
+    public LocalDate getVek() {
+        return vek;
+    }
+
+    /*public void setVek(LocalDate vek) {
+        this.vek = vek;
+    }*/
+
+    public void setVek(String vek) {
+        this.vek = LocalDate.parse(vek);
+    }
+
+    public Pohlavi getPohlavi() {
+        return pohlavi;
+    }
+
+    public void setPohlavi(Pohlavi pohlavi) {
+        this.pohlavi = pohlavi;
+    }
+
+    public Sport getSport() {
+        return sport;
+    }
+
+    public void setSport(Sport sport) {
+        this.sport = sport;
+    }
+
+    public String getTurnus() {
+        return turnus;
+    }
+
+    public void setTurnus(String turnus) {
+        this.turnus = turnus;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+    public EnumSet<Sport> getVyber() {
+        return vyber;
+    }
+
+    public void setVyber(EnumSet<Sport> vyber) {
+        this.vyber = vyber;
+    }
+}
